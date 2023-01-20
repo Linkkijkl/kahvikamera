7f957fa914095ac475feefbf2aaa28fad9370486
Kattila <toimistovastaava@linkkijkl.fi>
2023-01-20 15:02:52 +0200

    kuva kahvi_pannusta Fri Jan 20 15:02:52 EET 2023

--- kahvi2.sh
+++ kahvi2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sleep 30
+#sleep 30
 killall ffmpeg
 ffmpeg -y -f video4linux2 -i /dev/video0 -q:v 1 -vframes 1 ~/kahvikamera/kuva.jpg
 # scp -P 52222 ~/kahvikamera.jpg kahvikamera@loota.xyz:~/kahvikamera.jpg
