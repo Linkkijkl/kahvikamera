5dc65c22231038212ad051c57bd6a5f02325b191
Kattila <toimistovastaava@linkkijkl.fi>
2023-01-20 15:01:33 +0200

    skripti v1

--- kahvi2.sh
+++ kahvi2.sh
@@ -2,5 +2,9 @@
 
 sleep 30
 killall ffmpeg
-ffmpeg -y -f video4linux2 -i /dev/video0 -q:v 1 -vframes 1 ~/kahvikamera.jpg
-scp -P 52222 ~/kahvikamera.jpg kahvikamera@loota.xyz:~/kahvikamera.jpg
+ffmpeg -y -f video4linux2 -i /dev/video0 -q:v 1 -vframes 1 ~/kahvikamera/kuva.jpg
+# scp -P 52222 ~/kahvikamera.jpg kahvikamera@loota.xyz:~/kahvikamera.jpg
+
+# koneelle oma githubkäyttäjä?
+git commit -am "kuva kahvi_pannusta `date`"
+git push
