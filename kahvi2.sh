43d8e4fd6bbf854ba7bb7a26dff90efd16b2edb5
Kattila <toimistovastaava@linkkijkl.fi>
2023-01-20 15:03:39 +0200

    kuva kahvi_pannusta Fri Jan 20 15:03:39 EET 2023

--- kahvi2.sh
+++ kahvi2.sh
@@ -5,6 +5,7 @@ killall ffmpeg
 ffmpeg -y -f video4linux2 -i /dev/video0 -q:v 1 -vframes 1 ~/kahvikamera/kuva.jpg
 # scp -P 52222 ~/kahvikamera.jpg kahvikamera@loota.xyz:~/kahvikamera.jpg
 
-# koneelle oma githubkäyttäjä?
+sleep 5
+# koneelle oma githubkäyttäjä? deploy keyllä onnistui
 git commit -am "kuva kahvi_pannusta `date`"
 git push
