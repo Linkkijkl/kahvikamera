c5f3fbffee29a0639191b360e88bccdefd7031e2
Alexander Goldhill <alejgold@jyu.fi>
2024-08-04 22:38:33 +0300

    refactor: rewrite halukkaat tracking using queue

--- web/app.py
+++ web/app.py
@@ -1,56 +1,41 @@
 from flask import Flask, render_template, request, Response
 import time
+from queue import Queue
 
 app = Flask(__name__)
-halukkaat = []
-HALUKKAATMAX = 10
 
-#========= tietokanta =========
+def virkista_halukkaat(halukkaat: Queue[float]):
+    # Queue's get() method pops the queue's next item, and there is
+    # no peek method this is why q.queue[0] is checked this way.
+    app.logger.info("Virkistetään halukkaat")
+    while not halukkaat.empty() \
+        and halukkaat.queue[0] - time.time() > 15*60:
+        halukkaat.get()
 
-def poista_vanhat_halukkaat():
-    global halukkaat
-    timestamp = time.time()
-    temp = []
-    for h in halukkaat:
-        if (timestamp - h < 15*60):
-            temp.append(h)
-    halukkaat = temp
 
-def lisaa_uusi_halukas():
-    global halukkaat
-    timestamp = time.time()
-    poista_vanhat_halukkaat()
-    if len(halukkaat) >= HALUKKAATMAX:
-        halukkaat.pop(0)
-    halukkaat.append(timestamp)
+def lisaa_uusi_halukas(halukkaat: Queue[float]):
+    virkista_halukkaat(halukkaat)
+    app.logger.info("Lisätään halukas")
+    halukkaat.put(time.time())
 
-#========= main =========
-
-def halukkaatstr():
-    halukkaatstr = "Halukkaat"
-    halukkaatlkm = len(halukkaat)
-    if   (halukkaatlkm == 3):  halukkaatstr = halukkaatstr + f" :{halukkaatlkm}"
-    elif (halukkaatlkm >= HALUKKAATMAX): halukkaatstr = halukkaatstr + f": {halukkaatlkm} (max)"
-    else:                      halukkaatstr = halukkaatstr + f": {halukkaatlkm}"
-    return halukkaatstr
 
 @app.route('/kahvi',  methods=['POST'])
 def home():
-    if len(halukkaat) >= HALUKKAATMAX:
-        halukkaats = halukkaatstr() 
+    lkm = halukkaat.qsize()
+    if lkm >= HALUKKAATMAX:
+        halukkaats = halukkaatstr(lkm)
         return render_template('index.html', halukkaat=halukkaats), 418
 
-    lisaa_uusi_halukas()
+    lisaa_uusi_halukas(halukkaat)
     x = render_template('redirect.html')
     resp = Response(x)
     resp.headers.add('Location', 'https://kattila.cafe')
     return resp
 
 @app.route('/', methods=['GET'])
-def kissa():
-    poista_vanhat_halukkaat()
-    halukkaats = halukkaatstr() 
-    return render_template('index.html', halukkaat=halukkaats)
+    virkista_halukkaat(halukkaat)
+    lkm = halukkaat.qsize()
+    halukkaats = halukkaatstr(lkm)
 
 @app.route('/favicon.ico')
 def favicon():
