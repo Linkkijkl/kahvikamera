639f3a925cd0c9c3d4384f891e0cde6c96091ba5
Alexander Goldhill <73328090+45spoons@users.noreply.github.com>
2025-05-08 17:43:24 +0300

    feat: add interested data fetch

--- templates/js/reloads.js
+++ templates/js/reloads.js
@@ -1,8 +1,29 @@
-function updateImage() {
-    const imageUrl = "{{ api_host }}/coffee/image";
-    const newImageUrl = imageUrl + "?t=" + new Date().getTime();
+let interested_max = 0;
+
+const interestedMaxEndpoint = "{{ api_host }}/interested/max";
+fetch(interestedMaxEndpoint)
+.then(response => response.text())
+.then(interested_max_response => {
+    interested_max = interested_max_response;
+})
+
+function updateCoffeeImage() {
+    const coffeeImageEndpoint = "{{ api_host }}/coffee/image";
+    const newImageUrl = coffeeImageEndpoint + "?t=" + new Date().getTime();
 
     document.getElementById("coffee-image").src = newImageUrl;
 }
 
-setInterval(updateImage, 10000);
+function updateInterested() {
+    const interestedAmountEndpoint = "{{ api_host }}/interested/amount";
+    const interestedElement = document.getElementById("interested");
+
+    fetch(interestedAmountEndpoint)
+    .then(response => response.text())
+    .then(interested_amount => {
+        interestedElement.textContent = `Halukkaat: ${interested_amount}/${interested_max}`;
+    })
+}
+
+setInterval(updateCoffeeImage, 10000);
+setInterval(updateInterested, 10000);
