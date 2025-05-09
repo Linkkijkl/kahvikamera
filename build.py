d1e03ccf1b4d1b511596a3c6cba288d9fe38bbf5
Lari Nurkka <lari.nurkka@gmail.com>
2025-05-09 16:37:20 +0300

    Fix deployed sites

--- build.py
+++ build.py
@@ -3,9 +3,8 @@ from staticjinja import Site
 
 if __name__ == "__main__":
     env_globals = {
-        "api_host": "http://localhost:5100"
+        "api_host": "https://kattila-api.linkkijkl.fi"
     }
     site = Site.make_site(outpath="public", env_globals=env_globals)
     # enable automatic reloading
     site.render()
-    
