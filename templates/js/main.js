5c79c13be9e5fda44edeb456ae07a2f3e698a4c7
Lari Nurkka <lari.nurkka@gmail.com>
2025-05-10 13:36:56 +0300

    Clean script

---
+++ templates/js/main.js
@@ -0,0 +1,40 @@
+let interestedMax = 0;
+
+const updateImage = () => {
+    const coffeeImageEndpoint = '{{ api_host }}/coffee/image';
+    const newImageUrl = coffeeImageEndpoint + '?t=' + new Date().getTime();
+    document.getElementById('coffee-image').src = newImageUrl;
+};
+
+const updateInterested = () => {
+    const interestedAmountEndpoint = '{{ api_host }}/interested/amount';
+    const interestedElement = document.getElementById('interested');
+    fetch(interestedAmountEndpoint)
+        .then(response => response.text())
+        .then(interestedAmount => {
+            interestedElement.textContent = `Halukkaat: ${interestedAmount}/${interestedMax}`;
+        });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const interestedMaxEndpoint = '{{ api_host }}/interested/max';
+    fetch(interestedMaxEndpoint)
+        .then(response => response.text())
+        .then(interestedMaxResponse => {
+            interestedMax = interestedMaxResponse;
+        });
+
+    const updateInterval = 10_000;
+    setInterval(() => {
+        updateImage();
+        updateInterested();
+    }, updateInterval);
+});
+
+document.getElementById('coffee-button').addEventListener('click', () => {
+    const interestedPostEndpoint = '{{ api_host }}/interested';
+    fetch(interestedPostEndpoint, {
+        method: 'POST'
+    });
+    updateInterested();
+});
