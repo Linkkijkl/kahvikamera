ed15114e1ab403f60d2be6475955f91712afc717
Alexander Goldhill <73328090+45spoons@users.noreply.github.com>
2025-09-01 13:12:29 +0300

    feat: human readable build time in global env

--- build.py
+++ build.py
@@ -1,10 +1,12 @@
 from staticjinja import Site
 import shutil
+import time
 
 
 if __name__ == "__main__":
     env_globals = {
-        "api_host": "https://kattila-api.linkkijkl.fi"
+        "api_host": "https://kattila-api.linkkijkl.fi",
+        "build_time": time.ctime()
     }
     site = Site.make_site(outpath="public", env_globals=env_globals)
     site.render()
